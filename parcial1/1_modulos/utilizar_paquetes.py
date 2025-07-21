a4a316654e438bd1ab2e01599203a02836f47f40
PerezAddy <addymaaxperezorozco11@gmail.com>
2025-07-21 12:25:01 -0600

    Delete parcial1 directory

--- parcial1/1_modulos/utilizar_paquetes.py
+++
@@ -1,8 +0,0 @@
-from paquete1 import modulos
-
-print(modulos.saludar("Daniel Contreras Ruano"))
-
-modulos.borrarPantalla()
-nom,tel=modulos.solicitarDatos()
-print(f".: Agenda Telefonica:  nombre: {nom}  telefono: {tel}" )
-modulos.espereTecla()
